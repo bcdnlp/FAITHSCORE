dc2932a05e1cdc603021f370c17990abab8b7e3f
artemisp <artemisp@seas.upenn.edu>
2024-05-07 12:46:16 -0400

    Batch and fix lamma use

--- src/faithscore/utils.py
+++ src/faithscore/utils.py
@@ -14,15 +14,21 @@ def blip_2(image_path, prompt, model, vis_processors):
     return ans[0]
 
 def ofa(entail, model, prompt, image):
-    input = {'image': image,
-             'text': prompt}
+    if isinstance(image, list) and len(image) == 1:
+        image = image[0]
+    elif isinstance(image[0], list):
+        image = [img[0] for img in image]
+    BS = len(prompt)
+        
+    input = [{'image': im,
+             'text': p} for im,p in zip(image, prompt)]
 
     # print(input)
-    output = model(input)
+    output = [model(inp) for inp in input]
     if entail:
-        ans = output[OutputKeys.LABELS][0]
+        ans = [out[OutputKeys.LABELS][0] if isinstance(out[OutputKeys.LABELS], list) else out[OutputKeys.LABELS] for out in output]
     else:
-        ans = output[OutputKeys.TEXT][0]
+        ans = [out[OutputKeys.TEXT][0] if isinstance(out[OutputKeys.TEXT], list) else out[OutputKeys.TEXT] for out in output]
     return ans
 
 def mplug(image, prompt, mplug_model):
